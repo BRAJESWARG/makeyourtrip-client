798d00eba5980d69c3c3da5877b65b930a829db4
BRAJESWAR GHOSH <96302225+BRAJESWARG@users.noreply.github.com>
2025-01-28 19:20:42 +0530

    Update MultiCarousel.js

--- src/Components/Home-Section/MultiCarousel.js
+++ src/Components/Home-Section/MultiCarousel.js
@@ -1,39 +1,40 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Card from 'react-bootstrap/Card';
+// import CardGroup from 'react-bootstrap/CardGroup';
+// import Card from 'react-bootstrap/Card';
 import './CardCarousel.css';
 
 
 function MultiCarousel() {
     return (
-        <div style={{ display: 'block', width: '70%', padding: 30, alignItems: 'center' }}>
-            <h4>React-Bootstrap Carousel Component</h4>
-            <Carousel>
-                <Carousel.Item interval={1500}>
-                    <img
-                        className="d-block w-100"
-                        src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-                        alt="ImageOne"
-                    />
-                    <Carousel.Caption>
-                        <h3>Label for first slide</h3>
-                        <p>Sample Text for Image One</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="d-block w-100"
-                        src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-                        alt="ImageTwo"
-                    />
-                    <Carousel.Caption>
-                        <h3>Label for second slide</h3>
-                        <p>Sample Text for Image Two</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={1500}>
+        <div className='MultiCarousel'>
+            <div style={{ display: 'block', width: '70%', padding: 30, alignItems: 'center' }}>
+                <h4>React-Bootstrap Carousel Component</h4>
+                <Carousel>
+                    <Carousel.Item interval={1500}>
+                        <img
+                            className="d-block w-100"
+                            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                            alt="ImageOne"
+                        />
+                        <Carousel.Caption>
+                            <h3>Label for first slide</h3>
+                            <p>Sample Text for Image One</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img
+                            className="d-block w-100"
+                            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+                            alt="ImageTwo"
+                        />
+                        <Carousel.Caption>
+                            <h3>Label for second slide</h3>
+                            <p>Sample Text for Image Two</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    {/* <Carousel.Item interval={1500}>
                     <div className='MultiCard-container'>
                         <CardGroup>
                             <Card>
@@ -69,8 +70,9 @@ function MultiCarousel() {
                             </Card>
                         </CardGroup>
                     </div>
-                </Carousel.Item>
-            </Carousel>
+                </Carousel.Item> */}
+                </Carousel>
+            </div>
         </div>
     );
 }
