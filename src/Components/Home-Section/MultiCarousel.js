deb51aed942fb80edcc3454b965b6d17754e7d79
BRAJESWAR GHOSH <96302225+BRAJESWARG@users.noreply.github.com>
2025-01-28 17:21:39 +0530

    Update MultiCarousel.js

--- src/Components/Home-Section/MultiCarousel.js
+++ src/Components/Home-Section/MultiCarousel.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/Card';
+import './CardCarousel.css';
+
 
 function MultiCarousel() {
     return (
-        <div style={{ display: 'block', width: 700, padding: 30 }}>
+        <div style={{ display: 'block', width: '70%', padding: 30, alignItems: 'center' }}>
             <h4>React-Bootstrap Carousel Component</h4>
             <Carousel>
                 <Carousel.Item interval={1500}>
@@ -28,6 +32,43 @@ function MultiCarousel() {
                         <h3>Label for second slide</h3>
                         <p>Sample Text for Image Two</p>
                     </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={1500}>
+                    <div className='MultiCard-container'>
+                        <CardGroup>
+                            <Card>
+                                <Card.Img className='CardImg' variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/de/11/48/mohan-mahal.jpg?w=900&h=500&s=1" />
+                                <div>
+                                    <Card.Body>
+                                        <Card.Title>Up to 15% OFF* on MOHAN MAHAL, Jaipur!</Card.Title>
+                                        <Card.Text>
+
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <small className="text-muted">Valid till: 31st Jan ’25</small>
+                                    </Card.Footer>
+                                </div>
+                            </Card>
+                            <br />
+                        </CardGroup>
+                        <CardGroup>
+                            <Card>
+                                <Card.Img className='CardImg' variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/de/11/48/mohan-mahal.jpg?w=900&h=500&s=1" />
+                                <div>
+                                    <Card.Body>
+                                        <Card.Title>Up to 15% OFF* on MOHAN MAHAL, Jaipur!</Card.Title>
+                                        <Card.Text>
+
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <small className="text-muted">Valid till: 31st Jan ’25</small>
+                                    </Card.Footer>
+                                </div>
+                            </Card>
+                        </CardGroup>
+                    </div>
                 </Carousel.Item>
             </Carousel>
         </div>
