33449576b11f3679e1566ee65c84f4a5d99c1726
Brajeswar Ghosh <brajeswar.ghosh@mihup.com>
2025-03-14 14:24:19 +0530

    updated InsideNav.js

--- src/Components/InsideHotel/InsideNav.js
+++ src/Components/InsideHotel/InsideNav.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from "react-scroll";
 import "./InsideNav.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 function InsideNav() {
 
+    const [activeItem, setActiveItem] = useState(null);
 
+    const navItems = [
+        { id: "StayOptions", label: "Stay Options" },
+        { id: "Amenities", label: "Amenities" },
+        { id: "Host", label: "Host" },
+        { id: "GuestReviews", label: "Guest Reviews" },
+        { id: "PropertyPolicies", label: "Property Policies" },
+        { id: "Location", label: "Location" },
+        { id: "SimilarProperties", label: "Similar Properties" },
+    ];
 
     return (
         <div className='InsideNav'>
@@ -14,39 +25,68 @@ function InsideNav() {
                     <ul className="Navigationstyles__MenuList-sc-1qhzy4a-2 dwFebw">
                         <li>
                             <div className="Navigationstyles__TextWrapperSpan-sc-1qhzy4a-3 llgclM">
-                                <a href="#stays" data-testid="navigation-header-cta-#stays" className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN">Stay Options</a>
+                                {navItems.map((item) => (
+                                    <Link
+                                        key={item.id}
+                                        to={item.id}
+                                        spy={true}
+                                        smooth={true}
+                                        duration={500}
+                                        className={`Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 ${activeItem === item.id ? "jyDmWNActiv" : "jyDmWN"
+                                            }`}
+                                        onClick={() => setActiveItem(item.id)}
+                                    >
+                                        {item.label}
+                                    </Link>
+                                ))}
+                                {/* <Link to="contact" spy={true} smooth={true} className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN" >
+                                    <button>Stay Options</button>
+                                </Link> */}
+                                {/* <a href="#stays" data-testid="navigation-header-cta-#stays" className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN">Stay Options</a> */}
+                            </div>
+                        </li>
+                        {/* <li>
+                            <div className="Navigationstyles__TextWrapperSpan-sc-1qhzy4a-3 llgclM">
+                                <Link to="contact" spy={true} smooth={true} className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN" >
+                                    <button>Amenities</button>
+                                </Link>
                             </div>
                         </li>
                         <li>
                             <div className="Navigationstyles__TextWrapperSpan-sc-1qhzy4a-3 llgclM">
-                                <a href="#amenities" data-testid="navigation-header-cta-#amenities" className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN">Amenities</a>
+                                <Link to="contact" spy={true} smooth={true} className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN" >
+                                    <button>Host</button>
+                                </Link>
                             </div>
                         </li>
                         <li>
                             <div className="Navigationstyles__TextWrapperSpan-sc-1qhzy4a-3 llgclM">
-                                <a href="#hosted-by" data-testid="navigation-header-cta-#hosted-by" className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN">Host</a>
+                                <Link to="contact" spy={true} smooth={true} className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN" >
+                                    <button>Guest Reviews</button>
+                                </Link>
                             </div>
                         </li>
                         <li>
                             <div className="Navigationstyles__TextWrapperSpan-sc-1qhzy4a-3 llgclM">
-                                <a href="#guest-reviews" data-testid="navigation-header-cta-#guest-reviews" className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN">Guest Reviews</a>
+                                <Link to="contact" spy={true} smooth={true} className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN" >
+                                    <button>Property Policies</button>
+                                </Link>
                             </div>
                         </li>
                         <li>
                             <div className="Navigationstyles__TextWrapperSpan-sc-1qhzy4a-3 llgclM">
-                                <a href="#hotel-policies" data-testid="navigation-header-cta-#hotel-policies" className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN">Property Policies</a>
+                                <Link to="contact" spy={true} smooth={true} className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN" >
+                                    <button>Location</button>
+                                </Link>
                             </div>
                         </li>
                         <li>
                             <div className="Navigationstyles__TextWrapperSpan-sc-1qhzy4a-3 llgclM">
-                                <a href="#location" data-testid="navigation-header-cta-#location" className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN">Location</a>
+                                <Link to="contact" spy={true} smooth={true} className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN" >
+                                    <button> Similar Properties</button>
+                                </Link>
                             </div>
-                        </li>
-                        <li>
-                            <div className="Navigationstyles__TextWrapperSpan-sc-1qhzy4a-3 llgclM">
-                                <a href="#similar-hotels" data-testid="navigation-header-cta-#similar-hotels" className="Navigationstyles__ItemLinkTo-sc-1qhzy4a-4 jyDmWN">Similar Properties</a>
-                            </div>
-                        </li>
+                        </li> */}
                     </ul>
                 </div>
             </section>
@@ -55,3 +95,54 @@ function InsideNav() {
 }
 
 export default InsideNav;
+
+// import React from "react";
+// import "./InsideNav.css";
+// import { Link } from "react-scroll";
+// const InsideNav = () => {
+//   return (
+//     <div className="n-wrapper" id="InsideNav">
+//       {/* left */}
+//       <div className="n-left">
+//         <div className="n-name">Brajeswar</div>
+//       </div>
+//       {/* right */}
+//       <div className="n-right">
+//         <div className="n-list">
+//           <ul style={{ listStyleType: "none" }}>
+//             <li>
+//               <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+//                 Home
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="services" spy={true} smooth={true}>
+//                 Serivces
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="works" spy={true} smooth={true}>
+//                 Experience
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="portfolio" spy={true} smooth={true}>
+//                 Protfolio
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="testimonial" spy={true} smooth={true}>
+//                 Testimonial
+//               </Link>
+//             </li>
+//           </ul>
+//         </div>
+//         <Link to="contact" spy={true} smooth={true}>
+//           <button className="button n-button">Contact</button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default InsideNav;
